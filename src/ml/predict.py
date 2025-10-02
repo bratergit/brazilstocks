65ff5453868277acd54b20845cbed40a7a2bdc33
Paridhi <pari9901@gmail.com>
2025-10-02 19:59:46 +0530

    Add: placeholder predict_price function (closes #19)

---
+++ src/ml/predict.py
@@ -0,0 +1,8 @@
+# src/ml/predict.py
+
+def predict_price(ticker):
+    """
+    Placeholder function to predict stock price.
+    Can be implemented later with a machine learning model (e.g., Linear Regression).
+    """
+    pass  # To be implemented later
